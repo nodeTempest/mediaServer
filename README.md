efa8bbee6a4bec18694212eadcc8f2eb7712c776
nodeTempest <49026266+nodeTempest@users.noreply.github.com>
2019-08-16 13:23:54 +0300

    Create readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# projectLul
+### Softserv project for luls.
+### Use [Wiki](https://github.com/nodeTempest/projectLul/wiki) for detailed in formation.
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,43 +1,15 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const jwt = require("jsonwebtoken")
 require("dotenv/config")
 
 const auth = (req, res, next) => {
     const token = req.header("x-auth-token")
-    if(!token) return res.status(401).json({ msg: "No token, authorization deinied" })
+    if (!token)
+        return res.status(401).json({ msg: "No token, authorization deinied" })
     try {
         const decoded = jwt.decode(token, process.env.JWT_KEY)
         req.user = decoded.user
         next()
-    } catch(err) {
+    } catch (err) {
         return res.status(400).json({ msg: "Token is invalid" })
     }
 }
